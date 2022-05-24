2a8e30d8be1f1bcefe072b315b034b59ac6c3e9e
8790fahad <8790fahadado@gmail.com>
2022-05-24 19:22:12 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useCallback, useState } from "react";
+import { Container, Nav } from "react-bootstrap";
+import { login, logout as destroy, accountBalance }  from "./utils/helper";
+import coverImg from "./image/sandwich.jpg"
+import "./App.css";
+import Wallet from "./component/Wallet";
+import Cover from "./utils/Cover";
+import { Notification } from "./utils/Notification";
+import Products from "./component/marketplace/Products";
 
-function App() {
+const App = function AppWrapper() {
+  const account = window.walletConnection.account();
+  const [balance, setBalance] = useState("0");
+  const getBalance = useCallback(async () => {
+    if (account.accountId) {
+      setBalance(await accountBalance());
+    }
+  }, [account.accountId]);
+
+  useEffect(() => {
+    getBalance();
+  }, [getBalance]);
+  //..
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Notification />
+      {account.accountId ? (
+        <Container fluid="md">
+          <Nav className="justify-content-end pt-3 pb-5">
+            <Nav.Item>
+              <Wallet
+                address={account.accountId}
+                amount={balance}
+                symbol="NEAR"
+                destroy={destroy}
+              />
+            </Nav.Item>
+          </Nav>
+          <main>
+            <Products />
+          </main>
+        </Container>
+      ) : (
+        <Cover name="Street Food" login={login} coverImg={coverImg} />
+      )}
+    </>
   );
-}
+};
 
 export default App;
